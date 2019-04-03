9e750c81ceb2a894afefc4b3e0b50d9202405223
y.taniguchi <y.softvalley@gmail.com>
2019-04-04 00:19:23 +0900

    add getDirNames func

--- createfile.go
+++ createfile.go
@@ -2,19 +2,32 @@ package main
 
 import (
   "bufio"
+  "fmt"
   "io"
   "os"
-  "fmt"
+  "regexp"
+  "strings"
 )
 
 func main() {
   str := readLinesfromStdin()
-  fmt.Println(str)
+  contest, number, task := getDirNames(str)
+  fmt.Println(contest, number, task)
 }
 
 func readLinesfromStdin() string {
-  io.WriteString(os.Stdout, "<Contest name> <Question number>\n")
+  io.WriteString(os.Stdout, "e.g. ) ABC122 A. Double Helix\n")
   scanner := bufio.NewScanner(os.Stdin)
   scanner.Scan()
   return scanner.Text()
+}
+
+func getDirNames(rawStr string) (contest string, number string, task string) {
+  rep := regexp.MustCompile(`\s+`)
+  compStr := rep.Split(rawStr, -1)
+  contest = compStr[0][:3]
+  number = "T" + compStr[0][3:6]
+  nAlNum := regexp.MustCompile(`\W`)
+  task = nAlNum.ReplaceAllString(strings.ToLower(strings.Join(compStr[1:], "_")), "")
+  return
 }
