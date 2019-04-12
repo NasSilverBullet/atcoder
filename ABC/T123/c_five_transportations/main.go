3a5f4b6236b84117465f2d6545057f98b56a2330
y.taniguchi <y.softvalley@gmail.com>
2019-04-12 22:38:41 +0900

    rewrite ABC123 C. Five Transportations

--- ABC/T123/c_five_transportations/main.go
+++ ABC/T123/c_five_transportations/main.go
@@ -6,14 +6,13 @@ import (
 )
 
 func main() {
-  var n int
+  var n, a int
   fmt.Scan(&n)
-  A := make([]int, 5)
   min := math.MaxInt64
   for i := 0; i < 5; i++ {
-    fmt.Scan(&A[i])
-    if A[i] < min {
-      min = A[i]
+    fmt.Scan(&a)
+    if a < min {
+      min = a
     }
   }
   fmt.Println((n+min-1)/min + 4)
