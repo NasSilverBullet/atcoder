4660a7bffb5278ee6b20c98538e62df5bc18b4ff
nas <nas.silver.bullet@gmail.com>
2019-11-15 22:49:23 +0900

    abc087_b

--- abc087_b.py
+++ abc087_b.py
@@ -1,14 +1,39 @@
-# Me
+# # Me
 def count(a, b, c, x):
     A = 10
     B = 2
 
-    x /= 50
+    x //= 50
 
     count = 0
     for an in range(0, a+1):
         for bn in range(0, b+1):
             if 0 <= (x - (an * A + bn * B)) <= c:
+                count += 1
+
+    return count
+
+
+a = int(input())
+b = int(input())
+c = int(input())
+x = int(input())
+print(count(a, b, c, x))
+
+
+# Other
+def count(a, b, c, x):
+    A = 10
+    B = 2
+
+    x //= 50
+
+    count = 0
+    for an in range(min(a, x // A) + 1):
+        Y = x - A * an
+        for bn in range(min(b, Y // B) + 1):
+            Z = Y - B * bn
+            if Z <= c:
                 count += 1
 
     return count
