25a892b6469dc52937344b1f524723750591eb87
nas <nas.silver.bullet@gmail.com>
2019-11-16 00:35:58 +0900

    abc085_c

---
+++ abc085_c.py
@@ -0,0 +1,19 @@
+def get_pattern(n, y):
+    X = 10
+    Y = 5
+    Z = 1
+
+    y //= 1000
+    for nx in range(n+1):
+        m = n - nx
+        for ny in range(m+1):
+            nz = m - ny
+            sum = nx * X + ny * Y + nz * Z
+            if y == sum:
+                return nx, ny, nz
+    return -1, -1, -1
+
+
+n, y = [int(x) for x in input().split()]
+a, b, c = get_pattern(n, y)
+print(a, b, c)
