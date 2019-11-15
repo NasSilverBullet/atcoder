8608412d972698aab0d5b1df0c993afcb0834925
nas <nas.silver.bullet@gmail.com>
2019-11-15 17:35:52 +0900

    abc018_a

---
+++ abc081_a.py
@@ -0,0 +1,20 @@
+# Me
+def f():
+    numstr = input()
+    c = 0
+    for i in numstr:
+        if int(i) == 1:
+            c += 1
+    print(c)
+
+
+f()
+
+
+# Other
+def f():
+    s = input()
+    print(s.count('1'))
+
+
+f()
