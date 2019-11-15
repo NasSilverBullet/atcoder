8da558c4c48b3e18ef98efe5e5e3b55badd9cf8f
nas <nas.silver.bullet@gmail.com>
2019-11-15 23:23:23 +0900

    abc083_b

---
+++ abc083_b.py
@@ -0,0 +1,16 @@
+def check(x, a, b):
+    sum = 0
+    while x != 0:
+        sum += x % 10
+        x //= 10
+    if a <= sum <= b:
+        return True
+    return False
+
+
+n, a, b = list(map(int, input().split()))
+sum = 0
+for i in range(1, n+1):
+    if check(i, a, b):
+        sum += i
+print(sum)
