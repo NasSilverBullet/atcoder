85d052e8eca6218f8e7ec3dadf27fd4eab702e6b
nas <nas.silver.bullet@gmail.com>
2019-11-20 00:42:41 +0900

    main

--- main.go
+++ main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
-
-	"github.com/NasSilverBullet/atcoder/golang"
+	"os/exec"
 )
 
 func main() {
@@ -17,8 +17,26 @@ func main() {
 }
 
 func run() error {
-	if err := golang.Arc004_1(); err != nil {
+	cmd := exec.Command("sh", "-c", "~/.pyenv/versions/pypy3.6-7.2.0/bin/pypy3 ~/go/src/github.com/NasSilverBullet/atcoder/abc051_b.py")
+
+	stdin, err := cmd.StdinPipe()
+	if err != nil {
 		return err
 	}
+
+	if _, err := io.WriteString(stdin, "2 2"); err != nil {
+		return err
+	}
+
+	if err := stdin.Close(); err != nil {
+		return err
+	}
+
+	out, err := cmd.Output()
+	if err != nil {
+		return err
+	}
+
+	fmt.Print(string(out))
 	return nil
 }
